fb564c4bb05881d75958da3e437627ac50fd5ecd
CodeVision <codevisionevgen@gmail.com>
2024-08-01 20:59:45 +0300

    add contacts

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Providers } from "./providers";
 
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
+import { NavbarComponent } from "@/components/navbar";
 
 export const metadata: Metadata = {
   title: {
@@ -48,13 +49,14 @@ export default function RootLayout({
       </head>
       <body
         className={clsx(
-          "min-h-screen bg-gradient-to-b from-background to-secondary font-sans antialiased",
+          "min-h-screen bg-background font-sans antialiased",
           fontSans.variable,
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
-            <main className=" container mx-auto max-w-7xl pt-16 px-6 flex-grow z-[1]">
+            <NavbarComponent />
+            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow z-[1]">
               {children}
             </main>
             <footer className="w-full flex items-center justify-center py-3">
--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,22 @@
+import { GithubIcon, LinkedInIcon, UserShieldIcon } from "@/components/icons";
+
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "Telegram Graph Generator",
   description: "Html meta to Telegram Graph",
-  navItems: [],
+  contacts: [
+    {
+      link: "https://portfolio-codevisionevgen.vercel.app",
+      icon: UserShieldIcon,
+    },
+    {
+      link: "https://github.com/CodeVisionEvgen",
+      icon: GithubIcon,
+    },
+    {
+      link: "https://www.linkedin.com/in/evheniy-homenyuk-611b552b8/",
+      icon: LinkedInIcon,
+    },
+  ],
 };
